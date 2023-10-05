122b5c6871e274c570d828445385b36af910ce9f
Randy23123 <rlopez@appdev.yearup.org>
2023-10-05 16:16:27 -0400

    added age and food

--- src/main/java/com/pluralsight/SandwhichShop.java
+++ src/main/java/com/pluralsight/SandwhichShop.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class SandwhichShop {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What do you want Regular or Large? ");
+        String food = scanner.nextLine();
+        double foodprice =0;
+        if (food.equals("regular")) {
+            System.out.println("$" + 5.45);
+            foodprice = 5.45;
+        }
+        else if (food.equals("large")) {
+            System.out.println("$" + 8.95);
+
+            foodprice = 8.95;
+        }
+
+        System.out.print("whats your age?: ");
+        int age = scanner.nextInt();
+
+        if (age >= 65){
+            foodprice = foodprice *.20;
+
+        }
+        else if (age <= 17){
+            foodprice = foodprice *.10;
+        }
+
+    }
 }
